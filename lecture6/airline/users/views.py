932be55ce47d88c29f18aa0cca6d3904fe0fda7d
dangnam739 <47108512+dangnam739@users.noreply.github.com>
2020-07-28 17:22:09 +0700

    Update users views

--- lecture6/airline/users/views.py
+++ lecture6/airline/users/views.py
@@ -7,6 +7,8 @@ from django.urls import reverse
 def index(request):
     if not request.user.is_authenticated:
         return HttpResponseRedirect(reverse("login"))
+    return render(request, "users/user.html")
+
 
 def login_view(request):
     if request.method == "POST":
@@ -17,11 +19,15 @@ def login_view(request):
             login(request, user)
             return HttpResponseRedirect(reverse("index"))
         else:
-            render(request, "users/login.html", {
+            return render(request, "users/login.html", {
                 "message": "Invalid credentials."
             })
-    return render(request, "users/login.html")
+    else:
+        return render(request, "users/login.html")
 
 
 def logout_view(request):
-    pass
+    logout(request)
+    return render(request, "users/login.html", {
+        "message": "Logged out."
+    })
